5d5179253eada0999fc4bb4aa20c5a452629993c
MiguelCaAr <castroarredondo.hector@gmail.com>
2024-02-06 00:21:59 -0700

    Se estaba mandando el object en vez del value

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,7 +1,18 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { useProductsStore } from "@/stores/Products";
+
+const route = useRoute();
+const productID = ref();
+const productsStore = JSON.parse(useProductsStore().products);
+productID.value = route.params.id;
+console.log(productsStore);
+// const product = productsStore.find(product => product.)
+</script>
+
 <template>
   <h1>Product Name:</h1>
 </template>
-
-<script setup></script>
 
 <style scoped></style>
