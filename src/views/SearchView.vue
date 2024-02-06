3c6b1491b4f14990f193a19ab774a807b4f91e56
Miguel-CaAr <castroarredondio.hector@gmail.com>
2024-02-06 07:35:56 -0700

    Loading del renderizado de los productos

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,12 +1,17 @@
 <script setup>
+import { ref } from "vue";
 import ProductDisplay from "@/components/ProductDisplay.vue";
 import { useProductsStore } from "@/stores/Products";
+import Loading from "@/components/LoadingSearch.vue";
 
 const data = useProductsStore();
 </script>
 
 <template>
-  <template v-if="data.products">
+  <template v-if="!data.products">
+    <Loading />
+  </template>
+  <template v-else>
     <ProductDisplay />
   </template>
 </template>
