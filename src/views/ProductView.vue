75d7bc37bf3a9f11fb1c20ae131b4720aa9c6194
MiguelCaAr <castroarredondo.hector@gmail.com>
2024-02-06 00:31:47 -0700

    Ya se muestran los datos del producto individual

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -5,14 +5,17 @@ import { useProductsStore } from "@/stores/Products";
 
 const route = useRoute();
 const productID = ref();
-const productsStore = JSON.parse(useProductsStore().products);
+const productsStore = JSON.parse(useProductsStore().products).data;
 productID.value = route.params.id;
-console.log(productsStore);
-// const product = productsStore.find(product => product.)
+const product = productsStore.find((product) => product.product_id === productID.value);
+console.log(product);
 </script>
 
 <template>
-  <h1>Product Name:</h1>
+  <h1>{{ product.product_title }}</h1>
+  <h5>{{ product.product_description }}</h5>
+  <p>Oferta en {{ product.offer.store_name }}</p>
+  <p>Precio {{ product.offer.price }}</p>
 </template>
 
 <style scoped></style>
